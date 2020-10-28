569dac229161be91158f31c1f14cd25ad0739e73
tartakovskyi <vvt2001@ukr.net>
2020-10-28 15:23:49 +0200

    webpack

--- lesson_04/src/index.js
+++ lesson_04/src/index.js
@@ -1,3 +1,4 @@
 import Person from "./components/Person";
+import './scss/base.scss'
 
 const u = new Person("Bill");
--- lesson_04/webpack.config.js
+++ lesson_04/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports =  (env = {mode: 'development'})  => {
 	const isProduction = env.mode === 'production';
@@ -12,8 +13,31 @@ module.exports =  (env = {mode: 'development'})  => {
 		module: {
 			rules: [
 			{test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+				{
+					loader: MiniCssExtractPlugin.loader,
+				},
+				{
+					loader: "css-loader", options: {
+						sourceMap: true
+					}
+				},
+				{
+					loader: "sass-loader", options: {
+						sourceMap: true
+					}
+				}
+				]
+			},
 			]  
 		},
+		plugins: [
+		new MiniCssExtractPlugin({
+			filename: "css/style.css"
+		})
+		],
 		devServer: {
 			contentBase: path.join(__dirname, 'src'),
 			compress: true,
